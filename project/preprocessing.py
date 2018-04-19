bff874b66b8e7e6ca5d410f7cde1690a842c2ee8
Arnór Kristmundsson <arnorkr@hotmail.com>
2018-04-19 15:05:06 +0000

    Bætti við möguleika á að reikna deltur og vann að SVC

--- project/preprocessing.py
+++ project/preprocessing.py
@@ -21,7 +21,7 @@ The rest are the features
 """
 
 from python_speech_features import mfcc
-#from python_speech_features import delta
+from python_speech_features import delta
 import numpy as np
 import scipy.io.wavfile as wav
 
@@ -29,7 +29,9 @@ GENRES = np.array(['blues', 'classical', 'country', 'disco', 'hiphop', 'jazz',
                    'metal', 'pop', 'reggae', 'rock'])
 
 # Configurable parameters
-NUMCAT = 2      # number of categories to examine
+NUMCAT = 10     # number of categories to examine
+DELTAS = True   # wether to include deltas or not
+
 MINLEN = 660000 # Lengdin á stysta laginu
 WINLEN = 0.023  # analysis window length, 25ms is default, Eggert recommends 23
 WINSTEP = 0.01  # step between successive windows in seconds
@@ -39,6 +41,7 @@ NFFT = 512      # FFT size
 LOWFREQ = 300   # lowest mel filter band edge, 0 is default, JL recommends 300
 PREEMPH = 0.97  # apply preemphasis filter with PREEMPH as coefficient
 CEPLIFTER = 22  # apply a lifter to final cepstral coefficients
+DELTAS = False   #
 
 def calculate_features(input_file, genre):
     """
@@ -56,23 +59,29 @@ def calculate_features(input_file, genre):
     mfcc_feat = mfcc(data[0:MINLEN], sample_rate, WINLEN, WINSTEP, NUMCEP,
                      NFILT, NFFT, LOWFREQ, sample_rate/2, PREEMPH, CEPLIFTER)
 
-#    # Delta MFCC
-#    delta_mfcc_feat = delta(mfcc_feat, 2)
-#    # Delta delta MFCC
-#    delta_delta_mfcc_feat = delta(delta_mfcc_feat, 2)
-#
-#    # Collect the features into a single array
-#    features = np.concatenate((mfcc_feat, delta_mfcc_feat,
-#                               delta_delta_mfcc_feat)).flatten()
-    features = mfcc_feat.flatten()
+    if DELTAS:
+        # Delta MFCC
+        delta_mfcc_feat = delta(mfcc_feat, 2)
+        # Delta delta MFCC
+        delta_delta_mfcc_feat = delta(delta_mfcc_feat, 2)
+
+        # Collect the features into a single array
+        features = np.concatenate((mfcc_feat, delta_mfcc_feat,
+                                   delta_delta_mfcc_feat)).flatten()
+    else:
+        features = mfcc_feat.flatten()
 
     # Write the genre index along with the features to a file
     output_data = np.concatenate((np.array([genre]), features))
 
     return output_data
 
+# Number of features
+N_FEATURES = 35832
+if DELTAS:
+    N_FEATURES = 3*N_FEATURES
 # Collect features along with category index into a matrix
-DATA = np.zeros((100*NUMCAT, 35833))
+DATA = np.zeros((100*NUMCAT, N_FEATURES+1))
 
 for i in range(NUMCAT):
     for j in range(100):
@@ -83,5 +92,6 @@ for i in range(NUMCAT):
         fileName = "../waves/waves/"+GENRES[i]+zeros+str(j)+".wav"
         DATA[100*i+j] = calculate_features(fileName, i)
 
-np.save("../data/data_"+str(NUMCAT)+"_categories.npy", DATA)
+np.save("../data/data_"+str(NUMCAT)+"_categories_deltas_"+str(DELTAS)+".npy",
+        DATA)
 print("Done!")
--- project/svc.py
+++ project/svc.py
@@ -5,21 +5,45 @@ Created on Mon Apr 16 12:26:57 2018
 
 @author: Arnór Kristmundsson
 Train a Support Vector Classifier (SVC) an report error rate
+The first category indicates are:
+    0: blues
+    1: classical
+    2: country
+    3: disco
+    4: hiphop
+    5: jazz
+    6  metal
+    7: pop
+    8: reggae
+    9: rock
 """
 import numpy as np
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import confusion_matrix
 from sklearn.svm import SVC
+from sklearn.metrics import confusion_matrix
 
-# Configurable parameters
-NUMCAT = 2       # number of categories to examine
-TEST_SIZE = 0.25 # proportion of the dataset to include in the test split.
+# Configurable parameters for
+NUMCAT = 10          # number of categories to examine
+DELTAS = True        # wether to include deltas or not
+
+# Number of features
+N_FEATURES = 35832
+if DELTAS:
+    N_FEATURES = 3*N_FEATURES
+
+
+TEST_SIZE = 0.25     # proportion of the dataset to include in the test split.
+
+#C = 1                # Penalty parameter C of the error term.
+#GAMMA = 1/N_FEATURES # Kernel coefficient
 
 # Load data
-DATA = np.load("../data/data_"+str(NUMCAT)+"_categories.npy")
+DATA = np.load("../data/data_"+str(NUMCAT)+"_categories_deltas_"+str(DELTAS)
+               +".npy")
 CATEGORY = DATA[:, 0]
 FEATURES = DATA[:, 1:]
+#N_FEATURES = FEATURES.shape[1]
 
 # Scale features
 SCALER = StandardScaler()
@@ -29,12 +53,55 @@ X_SCALED = SCALER.fit_transform(FEATURES)
 X_TRAIN, X_TEST, Y_TRAIN, Y_TEST = train_test_split(X_SCALED, CATEGORY,
                                                     test_size=TEST_SIZE)
 
-CLF = SVC(C=1, kernel='linear')
+## Identify optimal values for C and gamma
+## array of 5 logarithmically spaced values for C from 0.01 to 100
+#COARSE_C_GRID = [0.0001, 0.01, 1, 100, 10000]
+## array of 5 logarithmically spaced  values for γ from 10^-6 to 10^-2
+##COARSE_GAMMA_GRID = [np.power(N_FEATURES,2), np.power(N_FEATURES, 1.5),
+##                   N_FEATURES, np.sqrt(N_FEATURES), 1]
+#COARSE_GAMMA_GRID = [1e-08, 1e-06, 0.0001, 0.01, 1]
+## Accuracy grid
+#COARSE_ACC_GRID = np.zeros((5, 5))
+#for i in range(5):
+#    for j in range(5):
+#        iterClf = SVC(C=COARSE_C_GRID[i], gamma=COARSE_GAMMA_GRID[j])
+#        iterClf.fit(X_TRAIN, Y_TRAIN)
+#        COARSE_ACC_GRID[i][j] = iterClf.score(X_TEST, Y_TEST)
+#print(COARSE_ACC_GRID)
+##[[ 0.08   0.08   0.08   0.08   0.08 ]
+## [ 0.08   0.08   0.08   0.08   0.08 ]
+## [ 0.08   0.256  0.34   0.1    0.1  ]
+## [ 0.272  0.536  0.36   0.1    0.1  ]
+## [ 0.532  0.536  0.36   0.1    0.1  ]]
+##
+## array of 3 logarithmically spaced values for C
+#FINE_C_GRID = [10, 100, 1000]
+## array of 4 logarithmically spaced  values for gamma
+#FINE_GAMMA_GRID = [1e-07, 3e-07, 1e-06, 3e-06, 1e-05]
+## Accuracy grid
+#FINE_ACC_GRID = np.zeros((3, 5))
+#for i in range(3):
+#    for j in range(5):
+#        iterClf = SVC(C=FINE_C_GRID[i], gamma=FINE_GAMMA_GRID[j])
+#        iterClf.fit(X_TRAIN, Y_TRAIN)
+#        FINE_ACC_GRID[i][j] = iterClf.score(X_TEST, Y_TEST)
+#print(FINE_ACC_GRID)
+
+#[[ 0.284  0.428  0.496  0.508  0.532]
+# [ 0.492  0.488  0.5    0.496  0.532]
+# [ 0.492  0.488  0.5    0.496  0.532]]
+
+# Good values for C and gamma
+C_GOOD = 100
+GAMMA_GOOD = 1e-05
+
+# Train and test the classifier
+CLF = SVC(C=C_GOOD, kernel='linear', gamma=GAMMA_GOOD)
 CLF.fit(X_TRAIN, Y_TRAIN)
 Y_PRED = CLF.predict(X_TEST)
 ERROR_RATE = sum((Y_TEST != Y_PRED)/(100*NUMCAT*TEST_SIZE))
 print("SVM test set error rate: ", ERROR_RATE, "%")
 
-# Confusion matrix, see https://en.wikipedia.org/wiki/Confusion_matrix
+# Confusion matrix
 CONFUSION_MATRIX = confusion_matrix(Y_TEST, Y_PRED)
 print(CONFUSION_MATRIX)
